4833e53ddb623dc8d1361bc014761beef317c014
fmsarp <fmsarp@gmail.com>
2024-01-16 07:38:11 -0500

    API successfully moved to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,65 +1,47 @@
-
-export const handleApiCall = (req, res) => {
-    // Personal Access Token (PAT) for authentication
-    const PAT = '***';
-    const USER_ID = 'fmsarp';
-    const APP_ID = 'Devtest';
-    const MODEL_ID = 'face-detection';
-    const IMAGE_URL = req.body.input;
-
-    // Creating a JSON payload for the Clarifai API
-    const raw = JSON.stringify({
-      "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
-      },
-      "inputs": [
-        {
-          "data": {
-            "image": {
-              "url": IMAGE_URL
-            }
-          }
-        }
-      ]
-    });
-
-    // Configuring the options for the API request
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT,
-        'Content-Type': 'application/json', // Specifying JSON content type
-      },
-      body: raw
-    };
+export function handleApiCall(res, req) {
+     // Personal Access Token (PAT) for authentication
+     const PAT = '***';
+     const USER_ID = 'fmsarp';
+     const APP_ID = 'Devtest';
+     const MODEL_ID = 'face-detection';
+     const IMAGE_URL = req.body.input;
+     // Creating a JSON payload for the Clarifai API
+     const raw = JSON.stringify({
+       "user_app_id": {
+         "user_id": USER_ID,
+         "app_id": APP_ID
+       },
+       "inputs": [
+         {
+           "data": {
+             "image": {
+               "url": IMAGE_URL
+             }
+           }
+         }
+       ]
+     });
+ 
+     // Configuring the options for the API request
+     const requestOptions = {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Authorization': 'Key ' + PAT,
+         'Content-Type': 'application/json', // Specifying JSON content type
+       },
+       body: raw
+     };
      // Making a fetch request to the Clarifai API
      fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`, requestOptions)
-     .then(response => response.json())
-     .then(result => {
-       // Extracting information about detected regions in the image
-       const regions = result.outputs[0].data.regions;
-
-       const faceData = regions.map(region => {
-         const boundingBox = region.region_info.bounding_box;
-         const width = req.body(Number(document.getElementById('inputimage').width));
-         const height = req.body(Number(document.getElementById('inputimage').height));
-
-         return {
-           topRow: boundingBox.top_row * height,
-           leftCol: boundingBox.left_col * width,
-           bottomRow: height - boundingBox.bottom_row * height,
-           rightCol: width - boundingBox.right_col * width,
-         };
-       });
-       setFaceData({faceData});
-     })
-     .catch(err => console.log("cannot fetch clarifi API"));
+       .then(res => res.json())
+       .then(responseData => {
+        const data = responseData;
+        res.json(data)
+       })
+       .catch(err => res.status(400).json('unable to work with API'))
 }
-    
-
-export const hangleImageEntries = (req, res, db) => {
+export function hangleImageEntries(req, res, db) {
     const { id } = req.body;
     db('users').where('id', '=', id)
         .increment('entries', 1)
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express, { response } from "express";
+import express from "express";
 import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 import register from "./controllers/register.js";
 import signin from "./controllers/signin.js";
 import profile from "./controllers/profile.js";
-import { hangleImageEntries, handleApiCall } from './controllers/image.js';
+import { handleApiCall, hangleImageEntries } from './controllers/image.js';
 
 
 const db = knex({
@@ -32,7 +32,7 @@ app.post("/signin", (req, res) => signin(req, res, db, bcrypt));
 app.post('/register', (req, res) => register(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile(req, res, db));
 app.put('/image', (req, res) => hangleImageEntries(req, res, db));
-app.post('/imageurl', (req, res) => handleApiCall(req, res));
+app.post('/imageurl', (req, res) => handleApiCall(res, req));
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
